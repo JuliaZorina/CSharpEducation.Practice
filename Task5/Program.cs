931983194642e6bccbd7ab610ba89a1e9af386d8
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-22 22:05:17 +0400

    Задание 5: считать из консоли две фразы по отдельности и вывести их вместе

---
+++ Task5/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var phrase1 = Console.ReadLine();
+            var phrase2 = Console.ReadLine();
+            Console.WriteLine(phrase1 + " " + phrase2);
+        }
+    }
+}

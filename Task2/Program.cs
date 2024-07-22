2cff5bc042d4cd95fb10702f52cac5f4692fb5f1
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-22 21:59:44 +0400

    Задание 2: объявление переменных разных видов

---
+++ Task2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            string companyName;
+            bool temp;
+            double weight;
+        }
+    }
+}

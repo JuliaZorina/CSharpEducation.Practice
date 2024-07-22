c97e568e843d7c4c95907eee96b533751480d3b2
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-22 22:01:28 +0400

    Задание 3: Объявление и инициализация переменных

---
+++ Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var age = 22;
+            var name = "Julia";
+            var temperature = 16.3;
+            var isFemale = true;
+        }
+    }
+}

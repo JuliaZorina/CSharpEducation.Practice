72fffd70a1f843d02ae56bef80aaef9c08f0e5e9
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-22 22:06:56 +0400

    Задание 6: вычисление площади круга

---
+++ Task6/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            const double pi = 3.14;
+            var r = 12.5;
+            Console.WriteLine("Площадь круга с радиусом = " + r + " равен " + pi * r * r);
+        }
+    }
+}

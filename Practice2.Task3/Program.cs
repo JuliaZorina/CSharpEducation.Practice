c4fd0e0604c89af705d0b7a0040959da7203b5a6
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 10:42:56 +0400

    Задание 3: Программа для вычисления выражения по заданной формуле

---
+++ Practice2.Task3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice2.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение переменной a");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной b");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение переменной f");
+            var f = double.Parse(Console.ReadLine());
+            if (a != 0)
+            {
+                Console.WriteLine($"Значение выражения равно: {(a + b - f / a) + f * a * a - (a + b)}");
+            }
+            else
+            {
+                Console.WriteLine("Введено некорректное значение переменной a. Значение должно быть !=0");
+            }
+
+        }
+    }
+}

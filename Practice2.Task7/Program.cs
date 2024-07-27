e9a51ff85fd371eca0a344cc010715da580e69d3
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 19:05:29 +0400

    Задание 7: перевод строк в разные регистры

---
+++ Practice2.Task7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Practice2.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MakeCapitalLetter();
+        }
+
+        public static void ToHigherRegister()
+        {
+            Console.WriteLine("Введите строку для перевода в верхний регистр");
+            var str = Console.ReadLine();
+            Console.WriteLine($"Преобразованная строка: {str.ToUpper()}");
+        }
+        public static void ToLowerRegister()
+        {
+            Console.WriteLine("Введите строку для перевода в нижний регистр");
+            var str = Console.ReadLine();
+            Console.WriteLine($"Преобразованная строка: {str.ToLower()}");
+        }
+        public static void MakeCapitalLetter()
+        {
+            Console.WriteLine("Введите слово");
+            var str = Console.ReadLine();
+            var letters = str.ToCharArray();
+            for(int i=0;i<letters.Length;i++)
+            {
+                if (i == 0)
+                {
+                    letters[0] = char.ToUpper(letters[0]);
+                }
+                else
+                {
+                    letters[i] = char.ToLower(letters[i]);
+                }
+                
+            }
+            Console.WriteLine($"Преобразованное слово: {new string(letters)}");
+        }
+    }
+}

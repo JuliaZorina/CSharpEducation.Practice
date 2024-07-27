ae5dc2c8b95c45def55fd58be301e16a93786444
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 11:28:14 +0400

    Задание 6: порграммный ввод строки и символа, подсчет процента вхождения заданного символа в строку

---
+++ Practice2.Task6/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice2.Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var s = "банан";
+            var symbol = 'а';
+            var counter = 0;
+            for(int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == symbol)
+                    counter++;
+
+            }
+            Console.WriteLine((double)counter/s.Length*100 + "%");
+        }
+    }
+}

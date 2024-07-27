a23420adecb92218cc4c9db956fe534b297f8037
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 19:22:51 +0400

    Задание 8: увеличение на 5 всех чисел, если есть 2 равных

---
+++ Practice2.Task8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice2.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите три числа");
+            var a = int.Parse(Console.ReadLine());
+            var b = int.Parse(Console.ReadLine());
+            var c = int.Parse(Console.ReadLine());
+            if (a == b || b == c || a == c)
+            {
+                a = a+5;
+                b = b+5;
+                c = c+5; 
+
+                Console.WriteLine($"{a }, {b }, {c }");
+            }
+            else
+                Console.WriteLine("Равных нет");
+        }
+    }
+}

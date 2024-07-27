95014ec3c076c63f6d895f86f148e4380407318c
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 11:16:17 +0400

    Задание 5: сравнение чисел

---
+++ Practice2.Task5/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice2.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение первого числа, которое вы хотите сравнить");
+            var a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите значение второго числа, которое вы хотите сравнить");
+            var b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Введите оператор сравнения:\n > - больше, \n < - меньше, \n = - равно, \n != - не равно");
+            var oper = Console.ReadLine();
+
+            switch (oper)
+            {
+                case ">":
+                    
+                        Console.WriteLine(a > b);
+                        break; 
+                case "<":
+                    Console.WriteLine(a < b);
+                    break;
+                case "=":
+                    Console.WriteLine(a == b);
+                    break;
+                case "!=":
+                    Console.WriteLine(a != b);
+                    break;
+
+            }
+        }
+    }
+}

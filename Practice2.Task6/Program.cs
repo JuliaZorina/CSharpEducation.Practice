fba0b198b8246ed08ec531827c84082c3b41fca2
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 18:36:58 +0400

    Задание 6:  ввод строки и символа из консоли(через аргументы командной строки) и подсчет процента вхождения символа в строку

--- Practice2.Task6/Program.cs
+++ Practice2.Task6/Program.cs
@@ -6,16 +6,44 @@ namespace Practice2.Task6
     {
         static void Main(string[] args)
         {
+            
+            var counter = 0;
+            for (int i = 0; i < args[0].Length; i++)
+            {
+                if (args[0][i] == args[1][0])
+                    counter++;
+
+            }
+            Console.WriteLine((double)counter / args[0].Length * 100 + "%");
+        }
+
+        private static void SymbolInString()
+        {
             var s = "банан";
             var symbol = 'а';
             var counter = 0;
-            for(int i = 0; i < s.Length; i++)
+            for (int i = 0; i < s.Length; i++)
             {
                 if (s[i] == symbol)
                     counter++;
 
             }
-            Console.WriteLine((double)counter/s.Length*100 + "%");
+            Console.WriteLine((double)counter / s.Length * 100 + "%");
         }
+        private static void UserSymbolInString()
+        {
+            var s = Console.ReadLine();
+
+            var symbol = Console.ReadLine()[0];
+            var counter = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == symbol)
+                    counter++;
+
+            }
+            Console.WriteLine((double)counter / s.Length * 100 + "%");
+        }
+
     }
 }

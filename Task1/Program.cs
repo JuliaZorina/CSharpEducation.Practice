b88b0ddb06b26219e5c51b785ccf549e74ac1f0d
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-27 10:08:35 +0400

    Цикл for, который выводит значение своего же счетчика

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice2.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            for(int i=0;i<10;i++)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}

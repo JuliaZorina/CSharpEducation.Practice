702c6e31fc345d55ca255004cb22439d28193946
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 18:21:56 +0400

    Задание 14: создание метода, который создает и возвращает пустой массив

---
+++ Practice2.Task14/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice2.Task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = CreateArray(10);
+            var rnd = new Random();
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next();   
+            }
+
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+
+        private static int[] CreateArray(int n)
+        {
+            return new int[n];
+        }
+    }
+}

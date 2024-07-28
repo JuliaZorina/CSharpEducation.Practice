3da8939341e4094ebaeb411a535a70b5dc9bee30
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 18:38:53 +0400

    Задание 16: создание метода, который принимает число по ссылке и изменяет его знак на противоположный

---
+++ Practice2.Task16/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = -1;
+            ChangeNumberSign(ref number);
+            Console.WriteLine(number);
+        }
+        private static void ChangeNumberSign(ref int number)
+        {
+            number = -number;
+        }
+    }
+}

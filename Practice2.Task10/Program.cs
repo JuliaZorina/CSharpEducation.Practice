08e4265fe12910b83653aa83ebae3230ac4f9ed6
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 16:51:55 +0400

    Задание 10: создание массива и его вывод в консоль. Размер массива задается через первый аргумент командной строки

---
+++ Practice2.Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice2.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[int.Parse(args[0])];
+            var randomInt = new Random();
+            for(int i=0; i < array.Length; i++)
+            {
+                array[i] = randomInt.Next();    
+            }
+            Console.WriteLine("Полученный массив:");
+            for(int i=0; i<array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}

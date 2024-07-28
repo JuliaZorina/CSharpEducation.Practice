bf0ac2741aafc91490a94c5f20e8c94528197515
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 18:29:20 +0400

    Задание 15: создание инвертированного массива

---
+++ Practice2.Task15/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2.Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[10];
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = i;
+            }
+
+            var invertedArray = InvertedArray(array);
+
+            Console.WriteLine("Инвертированный массив: ");
+            for(int i = 0; i < invertedArray.Length; i++)
+            {
+                Console.Write($"{invertedArray[i]} ");
+            }
+        }
+
+        private static int[] InvertedArray(int[] array)
+        {
+            var invertedArray = new int[array.Length];
+            for(int i=0; i < array.Length; i++)
+            {
+                invertedArray[invertedArray.Length-i-1] = array[i];
+            }
+            return invertedArray;
+        }
+    }
+}

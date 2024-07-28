861fe5f3661fa50e9133bd469cfda7e0d6b2b9c6
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 18:45:39 +0400

    Задание 17: метод принимает два числа и меняет их значения местами

---
+++ Practice2.Task17/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice2.Task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var a = 4;
+            var b = 12;
+            ReplaceNumbers(ref a, ref b);
+            Console.WriteLine($"{a}, {b}");
+        }
+        private static void ReplaceNumbers(ref int a, ref int b)
+        {
+            int temp;
+            temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}

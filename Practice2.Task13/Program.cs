9f9a0ce37220d469e0d10915e4c870c542bd7fd2
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 18:21:24 +0400

    Убраны ненужные комментарии

--- Practice2.Task13/Program.cs
+++ Practice2.Task13/Program.cs
@@ -31,7 +31,6 @@ namespace Practice2.Task13
             Console.WriteLine("Полученный двумерный массив: ");
             for(int i=0; i<array.GetLength(0); i++)
             {
-                //Console.WriteLine("----------------");
                 for(int j = 0; j < array.GetLength(1); j++)
                 {
                     Console.Write($"{array[i, j]} ");

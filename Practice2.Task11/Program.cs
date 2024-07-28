4871414b51e0fbb608442e61ee8f187433a10aa0
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 16:58:52 +0400

    Задание 11: создание массива и его вывод на экран, размер массива - первый аргумент командной строки, число, которым будет заполняться массиы - второй аргумент командной строки

---
+++ Practice2.Task11/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice2.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new int[int.Parse(args[0])];
+            int i = 0;
+            while (i < array.Length)
+            {
+                array[i] = int.Parse(args[1]);
+                i++;
+            }
+            i = 0;
+            Console.WriteLine("Полученный массив:");
+            do
+            {
+                Console.WriteLine(array[i]);
+                i++;
+            }
+            while (i < array.Length);
+        }
+    }
+}

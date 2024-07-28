2b7e9b3ba8a0909ff4b4d56db878637d28a136d8
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 16:41:07 +0400

    Задание 9: создан метод для создания массива целых чисел и вывода его на экран консоли. Длина массива передается в метод в качестве аргумента

---
+++ Task9/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice2.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var length = 10;
+            CreateArray(length);
+        }
+
+        private static void CreateArray(int length)
+        {
+            int[] array = new int[length];
+            var randomInt = new Random();
+            for(int i =0; i<array.Length; i++)
+            {
+                array[i] = randomInt.Next();
+            }
+            WriteArray(array);
+        }
+
+        private static void WriteArray(int[] array)
+        {
+            Console.WriteLine("Полученный массив: \n");
+            for(int i=0; i < array.Length; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+            Console.WriteLine();
+        }
+    }
+}

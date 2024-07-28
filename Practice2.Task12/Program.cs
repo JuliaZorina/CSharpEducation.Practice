ad0665b346a6cc29fa6c0dd1514ed196c78e9881
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-28 17:03:15 +0400

    Задание 12: создание массива строк, который заполняется пользователем

---
+++ Practice2.Task12/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice2.Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var array = new string[10];
+            Console.WriteLine("Введите строки для заполнения массива");
+            for(int i = 0; i < array.Length; i++)
+            {
+                array[i] = Console.ReadLine();
+            }
+            Console.WriteLine("Полученный массив строк:");
+            for(int i = 0; i < array.Length; i++)
+            {
+                Console.WriteLine(array[i]);
+            }
+        }
+    }
+}

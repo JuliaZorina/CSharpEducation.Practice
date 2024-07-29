43619cdb0ac18f9002a8868440c878779d6e08d2
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-29 14:32:25 +0400

    Задание 24: создание enum для дней недели, вывод на экран дней недели по названиям

---
+++ Practice2.Task24/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice2.Task24
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ShowWeekDay(WeekDay.Понедельник);
+            ShowWeekDay(WeekDay.Вторник);
+            ShowWeekDay(WeekDay.Среда);
+            ShowWeekDay(WeekDay.Четверг);
+            ShowWeekDay(WeekDay.Пятница);
+            ShowWeekDay(WeekDay.Суббота);
+            ShowWeekDay(WeekDay.Воскресенье);
+        }
+        public enum WeekDay
+        {
+            Понедельник,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        private static void ShowWeekDay(WeekDay day)
+        {
+            Console.WriteLine(day.ToString());
+        }
+
+    }
+}

fc545a843012b14358c8a136a78f95ed96a8b44c
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-29 13:40:29 +0400

    Задание 21: игра угадай число

---
+++ Practice2.Task21/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice2.Task21
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            GuessNumbergame();
+
+        }
+
+        private static void GuessNumbergame()
+        {
+            var rnd = new Random();
+            Console.WriteLine("Игра 'Угадай число'. Введите число от 1 до 100");
+            var userNumber = int.Parse(Console.ReadLine());
+            if (userNumber == rnd.Next(1, 100))
+            {
+                Console.WriteLine("Поздравляем! Вы угадали число!");
+            }
+            else
+            {
+                Console.WriteLine("К сожалению, в этот раз вы не угадали число:(");
+            }
+        }
+    }
+}

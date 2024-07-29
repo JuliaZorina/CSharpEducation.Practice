e62af1f524e7ac45876a44f1de0889ba4db870d9
Julia Zorina <yulia.a.zzo@gmail.com>
2024-07-29 14:06:45 +0400

    Задание 23: программа для вычисления високосного года

---
+++ Practice2.Task23/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice2.Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите номер года");
+            var year = int.Parse(Console.ReadLine());
+            if (year % 400 == 0)
+            {
+                Console.WriteLine($"{year} - високосный");
+            }
+            else if(year % 100 == 0)
+            {
+                Console.WriteLine($"{year} - невисокосный");
+            }
+            else if (year % 4 == 0)
+            {
+                Console.WriteLine($"{year} - високосный");
+            }
+            else
+            {
+                Console.WriteLine($"{year} - невисокосный");
+            }
+        }
+    }
+}

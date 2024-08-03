f4524148926fc673c7ef131e8722e43faaf4c7d3
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-03 10:17:40 +0400

    Создан класс "студент" и метод, который выводит в консоль данные о нем

---
+++ Practice3.Task1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var student = new Student();
+
+      student.StudentInfo();
+    }
+  }
+}
---
+++ Practice3.Task1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+  class Student
+  {
+    string name = "Julia";
+    int age = 22;
+
+    public void StudentInfo()
+    {
+      Console.WriteLine($"Имя: {name}. Возраст: {age}");
+    }
+  }
+}

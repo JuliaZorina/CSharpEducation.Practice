cdaaea1a7aa33505c77d611d55bc7f2bdb1137db
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-03 10:41:51 +0400

    Создан класс "Калькулятор", в нем созданы методы для арифметических операций, которые принимают 2 числа и возвращают результат

---
+++ Practice3.Task3/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Practice3.Task3
+{
+  class Calculator
+  {
+    public double Addition(int a, int b)
+    {
+      return a + b;
+    }
+
+    public double Subtraction(int a, int b)
+    {
+      return a - b;
+    }
+
+    public double Divide(int a, int b)
+    {
+      if (b == 0)
+      {
+        throw new ArgumentException("Нельзя делить на 0!");
+      }
+      else
+      {
+        return a / b;
+      }
+    }
+
+    public double Multiply(int a, int b)
+    {
+      return a * b;
+    }
+  }
+}
---
+++ Practice3.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice3.Task3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var calculator = new Calculator();
+
+      Console.WriteLine(calculator.Addition(5, 6));
+      Console.WriteLine(calculator.Subtraction(5, 6));
+      Console.WriteLine(calculator.Multiply(5, 6));
+      Console.WriteLine(calculator.Divide(5, 1));
+    }
+  }
+}

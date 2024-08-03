1f9eefb0db780ea8077ec6ca2cb0bc1a5411bfe4
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-03 11:00:24 +0400

    Создан класс "Книга" и два конструктора: один с параметрами, другой без параметров, который устанавливает значения по умолчанию

---
+++ Practice3.Task4/Book.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice3.Task4
+{
+  class Book
+  {
+    private string Author;
+    private string Title;
+    
+    public Book()
+    {
+      this.Author = "Имя Автора";
+      this.Title = "Название по умолчанию, которое я не придумала";
+    }
+
+    public Book(string author, string title)
+    {
+      this.Author = author;
+      this.Title = title;
+    }
+
+    public void BookInfo()
+    {
+      Console.WriteLine($"Автор: {this.Author}\nНазвание: {this.Title}");
+    }
+  }
+}
---
+++ Practice3.Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice3.Task4
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book(); //Конструктор по умолчанию
+
+      book.BookInfo();
+
+      var book2 = new Book("А.С. Пушкин", "Руслан и Людмила");
+
+      book2.BookInfo();
+    }
+  }
+}

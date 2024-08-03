c02595c960b1266988d1eaa9ecc2504065746cf8
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-03 10:31:25 +0400

    В класс "студент" добавлено свойство "средний балл", дополнен метод вывода данных о студенте

---
+++ Practice3.Task2/Program.cs
@@ -0,0 +1,24 @@
+﻿using Practice3.Task1;
+using System;
+
+namespace Practice3.Task2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var student = new Student();
+
+      Console.WriteLine("Введите имя студента");
+      student.name = Console.ReadLine();
+
+      Console.WriteLine("Введите возраст студента");
+      student.age = int.Parse(Console.ReadLine());
+
+      Console.WriteLine("Введите средний балл студента");
+      student.AverageScore = double.Parse(Console.ReadLine());
+
+      student.StudentInfo();
+    }
+  }
+}
---
+++ Practice3.Task2/Student.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+  class Student
+  {
+    public string name;
+    public int age;
+    private double averageScore;
+
+    public double AverageScore
+    {
+      get
+      {
+        return averageScore;
+      }
+      set
+      {
+        if(value<=0 || value >= 5)
+        {
+          Console.WriteLine("Средний балл не может быть меньше 0 или больше 5");
+        }
+        else
+        {
+          averageScore = value;
+        }
+      }
+    }
+
+    public void StudentInfo()
+    {
+      Console.WriteLine($"Имя: {name}.\nВозраст: {age}.\nСредний балл: {averageScore} ");
+    }
+  }
+}

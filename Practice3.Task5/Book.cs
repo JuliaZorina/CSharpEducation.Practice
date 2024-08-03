2833e34aca0bfdf38c57e0c319eed5455cfdfb3b
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-03 11:15:58 +0400

    Дополнен класс "Книга" из предыдущего задания, дополнены конструкторы, реализована цепочка конструкторов

---
+++ Practice3.Task5/Book.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Practice3.Task5
+{
+  class Book
+  {
+    private string Author;
+    private string Title;
+    private int PublicationDate;
+    
+    public Book()
+    {
+      this.Author = "Имя Автора";
+      this.Title = "Название по умолчанию, которое я не придумала";
+    }
+
+    public Book(string title): this(title, "Какой-то автор")
+    {
+      this.Title = title;
+    }
+
+    public Book(string title, string author) :this(title, author, 2020)
+    {
+      this.Title = title;
+      this.Author = author;
+    }
+
+    public Book(string author, string title, int publicationDate)
+    {
+      this.Author = author;
+      this.Title = title;
+      this.PublicationDate = publicationDate;
+    }
+
+    public void BookInfo()
+    {
+      Console.WriteLine($"Автор: {this.Author}\nНазвание: {this.Title}\nГод публикации: {PublicationDate}");
+    }
+  }
+}
---
+++ Practice3.Task5/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice3.Task5
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var book = new Book("Анна Каренниа");
+
+      book.BookInfo();
+    }
+  }
+}

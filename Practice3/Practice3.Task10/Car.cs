cbfae1685bc3da59715cd34b4eb5bfacb2004062
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-05 19:11:28 +0400

    Создан класс "Автомобиль" и метод, который выводит информацию о марке автомобиля в консоль

---
+++ Practice3/Practice3.Task10/Car.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace Practice3.Task10
+{
+  class Car
+  {
+    public string brand; 
+  }
+}
---
+++ Practice3/Practice3.Task10/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice3.Task10
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var car = new Car()
+      {
+        brand = "Porsche"
+      };
+      GetBrand(car);
+    }
+
+    public static void GetBrand(Car car)
+    {
+      Console.WriteLine($"Марка машины: {car.brand}");
+    }
+  }  
+}

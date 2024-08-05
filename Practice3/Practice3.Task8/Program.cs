fa99ae2f8ef53447aec81557291ad96103ffaec8
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-05 18:35:59 +0400

    Создана структура "Прямоуголник" и метод, который вычисляет его площадь

---
+++ Practice3/Practice3.Task8/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice3.Task8
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      /*var rectangle = new Rectangle()
+      {
+        width = 10,
+        height = 13
+      };*/
+
+      var rectangle = new Rectangle();
+      Console.WriteLine("Введите ширину прямоугольника");
+      rectangle.width = double.Parse(Console.ReadLine());
+      Console.WriteLine("Введите высоту прямоугольника");
+      rectangle.height = double.Parse(Console.ReadLine());
+
+      Console.WriteLine($"Площадь прямоугольника равна {GetRectangleArea(rectangle)}");
+    }
+
+    private static double GetRectangleArea(Rectangle rectangle)
+    {
+      return rectangle.height*rectangle.width;
+    }
+  }
+}
---
+++ Practice3/Practice3.Task8/Rectangle.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Practice3.Task8
+{
+  struct Rectangle
+  {
+    public double width;
+    public double height;
+  }
+}

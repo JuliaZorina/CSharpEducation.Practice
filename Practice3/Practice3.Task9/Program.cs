3d621f0c34022d59b1bced4979bd7e7e5fc6d316
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-05 19:02:08 +0400

    Создан класс "Студент" и метод, который меняет имя студента на "Аноним"

---
+++ Practice3/Practice3.Task9/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Practice3.Task9
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var student = new Student()
+      {
+        name = "Julia",
+        age = 22
+      };
+      Console.WriteLine($"Имя: {student.name}, возраст: {student.age}");
+      ChangeName(student);
+      Console.WriteLine($"Имя: {student.name}, возраст: {student.age}");
+    }
+
+    public static void ChangeName(Student student)
+    {
+      student.name = "Аноним";
+    }
+  }
+}
---
+++ Practice3/Practice3.Task9/Student.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Practice3.Task9
+{
+  class Student
+  {
+    public string name;
+    public int age;
+  }
+}

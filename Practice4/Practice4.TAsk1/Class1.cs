2253bfc54317b936c68df6c2445340592497f722
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-11 10:36:11 +0400

    Создана библиотека классов MathHelper

---
+++ Practice4/Practice4.TAsk1/Class1.cs
@@ -0,0 +1,27 @@
+﻿using System;
+namespace Practice4.Task1
+{
+  public class MathHelper
+  {
+    public int Add(int a, int b)
+    {
+      return a + b;
+    }
+    public int Multiply(int a, int b)
+    {
+      return a * b;
+    }
+    public int Substract(int a, int b)
+    {
+      return a - b;
+    }
+    public int Divide(int a, int b)
+    {
+      int res;
+      if (b == 0)
+        throw new ArgumentException("На ноль делить нельзя!");
+      else res = a / b;
+      return res;
+    }
+  }
+}

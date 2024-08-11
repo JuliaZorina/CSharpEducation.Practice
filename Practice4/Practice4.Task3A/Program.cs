08a66abfe371ea103196375e15c3cbc2569bd5ec
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-11 10:57:34 +0400

    Созданы объекты класса Person

---
+++ Practice4/Practice4.Task3A/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Practice4.Task3;
+
+namespace Practice4.Task3A
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var julia = new Person("Julia", 22);
+      julia.Print();
+
+      var oleg = new Person("Олег", 45);
+      oleg.Print();
+    }
+  }
+}

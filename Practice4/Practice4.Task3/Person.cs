2e60c7ad71b96937e165be0d57b03f97a577331d
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-11 10:55:34 +0400

    Создана библиотека с классом Person

---
+++ Practice4/Practice4.Task3/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+namespace Practice4.Task3
+{
+    public class Person
+    {
+    private string name;
+
+    public string Name
+    {
+      get 
+      { 
+        return name; 
+      }
+      set
+      {
+        if (string.IsNullOrEmpty(value))
+          throw new ArgumentException("Не существует человека без имени");
+        else
+        {
+          this.name = value;
+        }
+      }
+    }
+
+    private int age;
+
+    public int Age
+    {
+      get
+      {
+        return age;
+      }
+      set
+      {
+        if (value <= 0)
+          throw new ArgumentException("Возраст не может быть меньше 0!");
+        else
+        {
+          this.age = value;
+        }
+      }
+    }
+
+    public Person(string name,int age)
+    {
+      Name = name;
+      Age = age;
+    }
+
+    public void Print()
+    {
+      Console.WriteLine($"Имя: {name}.\nВозраст: {age}");
+    }
+
+    }
+}

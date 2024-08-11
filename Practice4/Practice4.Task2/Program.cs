dad3d75ec0a0064ebcc01209eb1d3ca7ccb98c71
Julia Zorina <yulia.a.zzo@gmail.com>
2024-08-11 10:37:58 +0400

    Создан класс, который использует методы библиотеки MathHelper

---
+++ Practice4/Practice4.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Practice4.Task1;
+
+namespace Practice4.Task2
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      var mathHelper = new MathHelper();
+      Console.WriteLine( mathHelper.Add(10, 5));
+      Console.WriteLine( mathHelper.Divide(10, 5));
+      Console.WriteLine( mathHelper.Multiply(10, 5));
+      Console.WriteLine( mathHelper.Substract(10, 5));
+    }
+  }
+}

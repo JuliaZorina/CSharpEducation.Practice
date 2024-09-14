14f3b59ef653d3103cbe480282060a5c6273d575
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-14 16:19:49 +0400

    Программа принимает 2 числа от пользователя и делит их, выводя результат. Ввод даных с клавиатуры.

---
+++ Practice6/Practice6.Task1/Divide.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice6.Task1
+{
+  public class Divide
+  {
+    public static int DevideInt(string a, string b)
+    {
+      int parseA;
+      int parseB;
+      int c;
+      try
+      {
+        parseA = int.Parse(a);
+        parseB = int.Parse(b);
+        c = parseA / parseB;
+      }
+      catch (DivideByZeroException ex)
+      {
+        throw new DivideByZeroException("Нельзя делить на ноль", ex);
+      }
+      catch (Exception ex) 
+      {
+        throw new Exception("Переменная a не является числом", ex);
+      } 
+      return c;
+    }
+  }
+}
---
+++ Practice6/Practice6.Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Practice6.Task1
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Введите число a");
+      var a = Console.ReadLine();
+      Console.WriteLine("Введите число b");
+      var b = Console.ReadLine();
+
+      try
+      {
+        Console.WriteLine($"Результат:{Divide.DevideInt(a, b)}");
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      finally
+      {
+        Console.WriteLine("Введите корректные данные");
+        Console.WriteLine("Введите число a");
+        a = Console.ReadLine();
+        Console.WriteLine("Введите число b");
+        b = Console.ReadLine();
+        Console.WriteLine($"Результат:{Divide.DevideInt(a, b)}");
+      }
+    }
+  }
+}

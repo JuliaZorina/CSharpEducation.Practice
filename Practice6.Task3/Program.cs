713248271e1317b0fc2a3ba6eb99c4cefea7d445
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-20 21:17:56 +0400

    Выполнено задание 3

---
+++ Practice6.Task3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Practice6.Task3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+      try
+      {
+        Console.WriteLine("Введите свое имя");
+        var user = new User(Console.ReadLine());
+        user.GetUserAge();
+      }
+      catch (ArgumentException ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      catch(TooManyAttemptsException ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+      catch (Exception ex)
+      {
+        Console.WriteLine(ex.ToString());
+      }
+    }
+  }
+}

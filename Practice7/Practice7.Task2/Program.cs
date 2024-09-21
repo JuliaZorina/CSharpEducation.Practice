862e880427403d736acaec0542370f13859209a2
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-21 16:28:46 +0400

    Создан класс Book, реализован интерфейс IComparable

--- Practice7/Practice7.Task2/Program.cs
+++ Practice7/Practice7.Task2/Program.cs
@@ -6,6 +6,18 @@ namespace Practice7.Task2
   {
     static void Main(string[] args)
     {
+      var person = new Person("Вася", 17);
+      var person2 = person.Clone() as Person;
+      Console.WriteLine(person2.Age);
+
+      var rect = new Rectangle();
+      rect.Width = 100;
+      rect.Height = 150;
+      var rect2 = rect.Clone() as Rectangle;
+      Console.WriteLine(rect.Width);
+      Console.WriteLine(rect.Height);
+
+
     }
   }
 }
---
+++ Practice7/Practice7.Task3/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Practice7.Task3
+{
+  internal class Program
+  {
+    static void Main(string[] args)
+    {
+    }
+  }
+}

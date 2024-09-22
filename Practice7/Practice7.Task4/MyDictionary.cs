c636204c5713464441ddeff792e0cc8a8ae7b040
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-22 16:12:55 +0400

    Выполнено задание 4

---
+++ Practice7/Practice7.Task4/MyDictionary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace Practice7.Task4
+{
+  public class MyDictionary : IEnumerator
+  {
+    public Dictionary<string, int> Items { get; set; }
+    private IEnumerator<KeyValuePair<string, int>> enumerator;
+
+    public object Current {
+      get
+      {
+        return this.enumerator.Current;
+      }
+    }
+
+    public bool MoveNext()
+    {
+      return this.enumerator.MoveNext();
+    }
+
+    public void Reset()
+    {
+      this.enumerator.Reset();
+    }
+
+    public MyDictionary()
+    {
+      this.Items = new Dictionary<string, int>();
+      this.enumerator = Items.GetEnumerator();
+    }
+  }
+}
---
+++ Practice7/Practice7.Task4/Numbers.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Practice7.Task4
+{
+  public class Numbers
+  {
+    public IEnumerable<int> GetEvenNumbers(int start, int end)
+    {
+      for (int i = start; i < end; i++)
+      {
+        if (i % 2 == 0)
+        {
+          yield return i;
+        }
+      }
+    }
+  }
+}

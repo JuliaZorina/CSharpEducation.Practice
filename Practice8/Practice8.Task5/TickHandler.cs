5ee03d3c8a209c54a7cbf31c16b885fe7ac5bf64
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-23 22:37:02 +0400

    Выполнено задание 5

---
+++ Practice8/Practice8.Task5/TickHandler.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice8.Task5
+{
+  public class TickHandler
+  {
+    public void Message(object sender, EventArgs e) 
+    {
+      var rnd = new Random();
+      Console.WriteLine($"Случайное число от 1 до 100: {rnd.Next(1, 100)}");
+    }
+
+    public void TickNumber(object sender, EventArgs e)
+    {
+      Console.WriteLine("Тик: " + DateTime.Now.ToString("HH:mm:ss"));
+    }
+  }
+}

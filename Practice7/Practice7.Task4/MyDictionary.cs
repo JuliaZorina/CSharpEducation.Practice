ccf3a0b6f402832cfb8b23bddef00a2cc8c40115
Julia Zorina <yulia.a.zzo@gmail.com>
2024-09-23 18:48:27 +0400

    Исправлена реализация IEnumerator

--- Practice7/Practice7.Task4/MyDictionary.cs
+++ Practice7/Practice7.Task4/MyDictionary.cs
@@ -1,35 +1,45 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace Practice7.Task4
 {
   public class MyDictionary : IEnumerator
   {
     public Dictionary<string, int> Items { get; set; }
-    private IEnumerator<KeyValuePair<string, int>> enumerator;
+    private int position = -1;
 
     public object Current {
       get
       {
-        return this.enumerator.Current;
+        if(position == -1 || position>=Items.Count) 
+          throw new ArgumentOutOfRangeException();
+        return this.Items.ElementAt(position);
       }
     }
 
     public bool MoveNext()
     {
-      return this.enumerator.MoveNext();
+      if(position >= Items.Count - 1)
+      {
+        position++;
+        return true;
+      }
+      else
+      {
+        return false;
+      }
     }
 
     public void Reset()
     {
-      this.enumerator.Reset();
+      position = -1;
     }
 
     public MyDictionary()
     {
       this.Items = new Dictionary<string, int>();
-      this.enumerator = Items.GetEnumerator();
     }
   }
 }
